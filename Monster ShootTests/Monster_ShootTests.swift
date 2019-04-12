7ca29dd82909a7bd1422055d1149328ea5067ea6
Igloo <igloo@dynamic.ipv6.sjtu.edu.cn>
2019-04-12 13:21:27 +0800

    Initial Commit

---
+++ Monster ShootTests/Monster_ShootTests.swift
@@ -0,0 +1,34 @@
+//
+//  Monster_ShootTests.swift
+//  Monster ShootTests
+//
+//  Created by Igloo on 2019/4/12.
+//  Copyright © 2019年 Igloo. All rights reserved.
+//
+
+import XCTest
+@testable import Monster_Shoot
+
+class Monster_ShootTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
